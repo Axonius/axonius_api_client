6cffeaf0fc15fea4f62213e67ac43bdbc1059ff9
Jim Olsen <jim@lifehack.com>
2020-01-22 12:58:20 -0500

    wip

--- axonshell_manual.py
+++ axonshell_manual.py
@@ -3,14 +3,35 @@
 """Utilities for this package."""
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import re
+
+import axonius_api_client as axonapi
+
+from axonius_api_client import tools
+
+# from axonius_api_client.cli.serial import join_cr, is_los, is_list, is_dos
+
+
+def key_field_match(key, fields):
+    """Pass."""
+    return any([re.search(field, key, re.I) for field in fields])
+
+
+def filter_row_field(obj, fields):
+    """Pass."""
+    return {k: v for k, v in obj.items() if not key_field_match(key=k, fields=fields)}
+
+
+def filter_rows_fields(raw_data, fields=None):
+    """Pass."""
+    raw_data = tools.listify(obj=raw_data, dictkeys=False)
+    fields = tools.listify(obj=fields)
+    return [filter_row_field(obj=r, fields=fields) for r in raw_data]
+
+
 if __name__ == "__main__":
     import os
 
-    import axonius_api_client as axonapi
-
-    import tabulate
-
-    tools = axonapi.tools
     axonapi.cli.cli_constants.load_dotenv()
 
     AX_URL = os.environ["AX_URL"]
@@ -43,8 +64,14 @@ if __name__ == "__main__":
     users = ctx.users
     adapters = ctx.adapters
 
-    compress_rows = axonapi.cli.serial.compress_rows
-    rows = devices.get(max_rows=2, fields_default=False, fields=["hostname"])
-    compressed = compress_rows(ctx=None, raw_data=rows)
-    table = tabulate.tabulate(compressed, showindex=False, headers="keys")
-    print(table)
+    obj_to_table = axonapi.cli.serial.obj_to_table
+    rows = devices.get(max_rows=1,)
+    filtered_rows = filter_rows_fields(
+        raw_data=rows, fields=["^adapters$", "internal_axon_id", "adapter_list_length"]
+    )
+    rows_table = obj_to_table(
+        ctx=None, raw_data=rows, joiner="\n", table_format="simple"
+    )
+    filtered_rows_table = obj_to_table(
+        ctx=None, raw_data=filtered_rows, joiner="\n", table_format="simple"
+    )
