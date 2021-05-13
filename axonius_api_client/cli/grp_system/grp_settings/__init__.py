04e3cc7c30d41c242a27e3efc47e15cec3d32575
Jim Olsen <jimbosan@gmail.com>
2021-05-13 10:32:55 -0400

    FR: CLI: added axonshell system settings-global configure-destroy

--- axonius_api_client/cli/grp_system/grp_settings/__init__.py
+++ axonius_api_client/cli/grp_system/grp_settings/__init__.py
@@ -3,15 +3,10 @@
 import click
 
 from ...context import AliasedGroup
-from . import (
-    cmd_get,
-    cmd_get_section,
-    cmd_get_subsection,
-    cmd_update_section,
-    cmd_update_section_from_json,
-    cmd_update_subsection,
-    cmd_update_subsection_from_json,
-)
+from . import (cmd_configure_destroy, cmd_get, cmd_get_section,
+               cmd_get_subsection, cmd_update_section,
+               cmd_update_section_from_json, cmd_update_subsection,
+               cmd_update_subsection_from_json)
 
 
 @click.group(cls=AliasedGroup)
@@ -43,3 +38,5 @@ for cmd in CMDS:
     settings_global.add_command(cmd.cmd)
     settings_lifecycle.add_command(cmd.cmd)
     settings_gui.add_command(cmd.cmd)
+
+settings_global.add_command(cmd_configure_destroy.cmd)
